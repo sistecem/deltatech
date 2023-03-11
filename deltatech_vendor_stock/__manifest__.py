4eeb70e64a974ca0f3fe134f9b15b70399a9ab4b
Dorin Hongu <dhongu@gmail.com>
2023-03-11 07:42:50 +0200

    Vendor stock

--- deltatech_vendor_stock/__manifest__.py
+++ deltatech_vendor_stock/__manifest__.py
@@ -6,14 +6,22 @@
 {
     "name": "Vendor Stock",
     "summary": "Vendor stock availability",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Warehouse",
-    "depends": ["product", "sale_stock", "deltatech_stock_inventory"],
+    "depends": [
+        "product",
+        "sale_stock",
+        # "deltatech_stock_inventory",
+    ],
     "license": "LGPL-3",
     "data": ["views/product_supplierinfo_view.xml", "views/sale_view.xml"],
-    "qweb": ["static/src/xml/qty.xml"],
+    "assets": {
+        "web.assets_qweb": [
+            "deltatech_vendor_stock/static/src/xml/**/*",
+        ],
+    },
     "images": ["static/description/main_screenshot.png"],
     "installable": True,
     "development_status": "Beta",
