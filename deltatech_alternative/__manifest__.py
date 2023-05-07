966658676bd8c84350b1e5e69146039af942b956
Dorin Hongu <dhongu@gmail.com>
2023-05-07 18:15:04 +0300

    eliminare

--- deltatech_alternative/__manifest__.py
+++ deltatech_alternative/__manifest__.py
@@ -1,11 +1,11 @@
-# ©  2008-2021 Deltatech
+# ©  2008-2023 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
 
 {
     "name": "Products Alternative",
-    "version": "15.0.1.0.7",
+    "version": "15.0.1.0.8",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Alternative product codes",
